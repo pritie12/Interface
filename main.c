28ae6005c1c4ccb668267e54577300049767e2e3
pritie12 <44751944+pritie12@users.noreply.github.com>
2018-12-17 15:33:56 +0100

    img_load
    aaa

--- main.c
+++ main.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
 {
 
     SDL_Init(SDL_INIT_EVERYTHING);
-    SDL_Surface *img = load_image(argv[1]);
+    SDL_Surface *img = IMG_LOAD(argv[1]);
     
     SDL_FreeSurface(img);
     SDL_Quit();
